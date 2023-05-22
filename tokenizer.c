d7fcb82b91bd7aeac6ffce6f09dc845fcf4db097
Sonter0077 <sontersimon29@gmail.com>
2023-05-22 02:41:36 -0700

    simple_shell updated

--- tokenizer.c
+++ tokenizer.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * **strtow - Splits the string into words. Repeated delimiters are ignored
+ * **strtow - Splits the string into words. Repeat delimiters are ignored
  * @str: the input string
  * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
@@ -9,15 +9,15 @@
 
 char **strtow(char *str, char *d)
 {
-	int a, b, c, d, numwords = 0;
+	int i, j, k, m, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	if (!d)
 		d = " ";
-	for (a = 0; str[a] != '\0'; a++)
-		if (!is_delim(str[a], d) && (is_delim(str[a + 1], d) || !str[a + 1]))
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
 
 	if (numwords == 0)
@@ -25,31 +25,31 @@ char **strtow(char *str, char *d)
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (a = 0, b = 0; b < numwords; b++)
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (is_delim(str[a], d))
-			a++;
-		c = 0;
-		while (!is_delim(str[a + c], d) && str[a + c])
-			c++;
-		s[b] = malloc((c + 1) * sizeof(char));
-		if (!s[b])
+		while (is_delim(str[i], d))
+			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
 		{
-			for (c = 0; c < b; c++)
-				free(s[c]);
+			for (k = 0; k < j; k++)
+				free(s[k]);
 			free(s);
 			return (NULL);
 		}
-		for (d = 0; d < c; d++)
-			s[b][d] = str[i++];
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
 		s[j][m] = 0;
 	}
-	s[b] = NULL;
+	s[j] = NULL;
 	return (s);
 }
 
 /**
- * **strtow2 - Splits the string into words
+ * **strtow2 - Split the string into words
  * @str: the input string
  * @d: the delimeter
  * Return: a pointer to an array of strings on success, or NULL on failure
